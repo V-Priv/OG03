a8896057c1d34ea1e81aa1730efb126c8e208b5b
V-Priv <vpriv10@gmail.com>
2024-08-20 11:58:42 +0300

    add new mode moving target

--- main.py
+++ main.py
@@ -21,6 +21,10 @@ target_y = random.randint(0, SCREEN_HEIGHT-target_height)
 
 color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
 
+# Переменные для движения мыши
+moving_mode = False
+velocity_x = 2  # скорость по оси X
+velocity_y = 2  # скорость по оси Y
 
 running = True
 
@@ -34,6 +38,17 @@ while running:
       if target_x < mouse_x < target_x + target_width and target_y < mouse_y < target_y + target_height:
           target_x = random.randint(0, SCREEN_WIDTH - target_width)
           target_y = random.randint(0, SCREEN_HEIGHT - target_height)
+    if event.type == pygame.KEYDOWN: # Переключение режима движения
+      if event.key == pygame.K_m: # Нажатие клавиши "M" для переключения режима
+        moving_mode = not moving_mode
+
+    if moving_mode:    # логика движения мишени
+      target_x += velocity_x
+      target_y += velocity_y
+      if target_x <= 0 or target_x >= SCREEN_WIDTH - target_width:
+          velocity_x = -velocity_x
+      if target_y <= 0 or target_y >= SCREEN_HEIGHT - target_height:
+          velocity_y = -velocity_y
 
     screen.blit( target_img, (target_x,target_y))
     pygame.display.update()
